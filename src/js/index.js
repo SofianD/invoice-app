88ee11703f135e297c771ff3adc3aa65fa6d793d
SofianD <doual.sofian@hotmail.com>
2021-03-20 17:56:53 +0100

    Code added to use the application as user.
    # window.onload:
    Get libraries in PARAMS array.
    
    # getTemplatesFrom():
    Get all templates from github API.
    
    # getTemplate():
    Get the model and the form chosen to transmit the data.
    
    # pushFormData():
    Push the form data and reset the form.
    
    # compileAndSave():
    Compile template with data. Then save it as PDF.
    To finaly return path of saved files.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 const invoicejs = require('@sofiand/invoice');
+// const app = require('electron')
+
 const PARAMS = [
     {
         name: 'invoiceJS-lib',
@@ -7,8 +9,13 @@ const PARAMS = [
     }
 ];
 let allTemplates = [];
+let data = [];
+let selectedTemplate, selectedForm, multiInputModel, nameOfMultiInput;
+let multiInputArr = []
 
 window.onload = async function() {
+    // console.log(app)
+    // app.ipcRenderer.postMessage()
     const ulLib = document.getElementById('libraries');
     for(let i = 0, l = PARAMS.length; i < l; i++) {
         const param = [
@@ -51,7 +58,7 @@ async function createElement(parent, element, arrayOfParams) {
 }
 
 async function getTemplatesFrom(el) {
-    allTemplates = []
+    allTemplates = [];
     const t = PARAMS.filter(x => x.id === el.target.id);
     try {
         allTemplates = await invoicejs.getListOfTemplates(t[0].url_api);
@@ -59,17 +66,66 @@ async function getTemplatesFrom(el) {
         console.log(error);
         return;
     }
-    // console.log(allTemplates);
-    // console.log(el)
     const a = document.getElementById('list-of-template');
     a.innerHTML = '';
     await createLI(allTemplates, a, getTemplate);
+
     return;
 }
 
 async function getTemplate(el) {
-    const t = allTemplates.filter(x => x.id === el.target.id);
-    console.log(t);
+    const t = allTemplates.filter(x => x.id === el.target.id)[0];
+    try {
+        selectedForm = await invoicejs.getForm(t.name);
+        selectedTemplate = await invoicejs.getTemplate(t.name);
+        const f = document.getElementById('template-form');
+        // console.log(selectedForm);
+        // console.log(selectedTemplate);
+        f.innerHTML = selectedForm;
+        selectedForm = document.getElementById('myForm');
+        selectedForm.onsubmit = pushFormData;
+        getInputArr();
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    return;
+}
+
+function getInputArr() {
+    const u = document.getElementsByClassName('multi-input')[0];
+    if(!u) {
+        console.log('inputModel error: ', u);
+        return;
+    }
+    multiInputModel = u.outerHTML;
+    nameOfMultiInput = u.id;
+    document.getElementById('add-param').onclick = pushdataInInputArr;
+}
+
+function pushdataInInputArr(event) {
+    const u = document.getElementsByClassName('multi-input')[0];
+    // console.log('u\n:', u);
+    const l = u.children.length;
+    let currentParam = {};
+    for(let i= 0; i < l; i++) {
+        const m = u.children[i].children.length;
+        for(let n = 0; n < m; n++) {
+            if(u.children[i].children[n].nodeName === 'INPUT') {
+                // currentArr.push({
+                //     name: u.children[i].children[n].name,
+                //     value: u.children[i].children[n].value
+                // });
+                currentParam[u.children[i].children[n].name] = u.children[i].children[n].value;
+            }
+        }
+    }
+    multiInputArr.push(currentParam);
+    u.outerHTML = multiInputModel;
+    // document.getElementById('add-param').onclick = pushdataInInputArr;
+    event.preventDefault();
+    return;
 }
 
 async function createLI(arr, parent, action) {
@@ -84,10 +140,6 @@ async function createLI(arr, parent, action) {
                 value: arr[i].name
             },
             {
-                key: 'id',
-                value: arr[i].name.split(' ').join('-')
-            },
-            {
                 key: 'onclick',
                 value: action
             },
@@ -95,10 +147,71 @@ async function createLI(arr, parent, action) {
                 key: 'style',
                 value: 'cursor: pointer; text-decoration: underline;'
             }
-        ]
-        console.log('create')
+        ];
+        // console.log('create')
         await createElement(parent, 'li', param);
         arr[i]['id'] = arr[i].name.split(' ').join('-');
     }
     return;
+}
+
+function pushFormData(event) {
+    // const selectedForm = document.getElementById('myForm');
+    const u = document.getElementsByClassName('multi-input')[0];
+    const formData = {};
+
+    if(u) {
+        // console.log('inputModel: ', u);
+        u.innerHTML = '';
+        formData[nameOfMultiInput] = multiInputArr;
+        multiInputArr = [];
+    }
+    const inputElmnts = ([...selectedForm.elements].flat()).filter(x => x.nodeName === 'INPUT');
+    // console.log(inputElmnts)
+    for(let i = 0, l = inputElmnts.length; i < l - 1; i++) {
+        const c = {
+            key: inputElmnts[i].name,
+            value: inputElmnts[i].value
+        };
+        formData[c.key] = c.value;
+    }
+
+    data.push(formData);
+    selectedForm.reset();
+    console.log(data);
+    u.outerHTML = multiInputModel;
+    event.preventDefault();
+    return;
+}
+
+async function getResponse(data) {
+    const h = document.getElementById('result');
+    
+    for(let i = 0, dl = data.length; i < dl; i++) {
+        const param = [
+            {
+                key: 'id',
+                value: data[i].name.split(' ').join('-')
+            },
+            {
+                key: 'innerHTML',
+                value: '<a target="_blank" href="'+ data[i].pathOfsavedFile +'">'+ data[i].name +'</a>'
+            },
+            {
+                key: 'style',
+                value: 'cursor: pointer; text-decoration: underline;'
+            }
+        ];
+
+        await createElement(h, 'li', param);
+    }
+
+    return;
+}
+
+async function compileAndSave() {
+    const response =  await invoicejs.getAndSaveInvoice(selectedTemplate, data);
+    console.log(response);
+    data = [];
+    getResponse(response);
 }
